5568588a6dd4931d5aa240c66b81725425f2de77
Brandon Winger-Air <brandonwinger-air@outlook.com>
2022-06-06 14:55:27 -0400

    create home route content & initial setup

--- src/components/Routes/Home.js
+++ src/components/Routes/Home.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+import millify from 'millify';
+import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
+
+const { Title } = Typography;
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <Title level={2} className="heading">Home</Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title="Currencies" value="5"/>
+        </Col>
+        <Col span={12}>
+          <Statistic title="Exchanges" value="5"/>
+        </Col>
+        <Col span={12}>
+          <Statistic title="Market Cap" value="5"/>
+        </Col>
+        <Col span={12}>
+          <Statistic title="Daily Volume" value="5"/>
+        </Col>
+        <Col span={12}>
+          <Statistic title="Markets" value="5"/>
+        </Col>
+      </Row>
+    </>
   )
 }
 
